9c140cc03b9cfc25b1cf538a391511df3081a19a
Thomas Nguyen <dnguyen001@berkeley.edu>
2024-04-02 20:25:40 -0700

    finished

--- components/cart.tsx
+++ components/cart.tsx
@@ -19,6 +19,17 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { useDebouncedCallback } from "use-debounce";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function Cart() {
   const addedProducts = useCartStore((state) => state.products);
@@ -49,68 +60,95 @@ export default function Cart() {
     addedProductIds.includes(parseInt(product.id))
   );
   return (
-    <Table className="max-w-[500px]">
-      <TableCaption>All transactions are final</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="md:w-[100px]">Item </TableHead>
-          <TableHead className="md:w-[200px]">Description </TableHead>
-          <TableHead>Quantity</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-          <TableHead className=""></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {filteredProducts.map((product: Product) => (
-          <TableRow key={product.title}>
-            <TableCell className="">
-              <div className=" aspect-square flex justify-center items-center bg-black overflow-hidden">
-                <Image
-                  src={product.thumbnail}
-                  width={100}
-                  height={100}
-                  alt={product.title}
-                  className="object-cover"
-                />
-              </div>
-            </TableCell>
-            <TableCell className="font-medium">{product.title}</TableCell>
-            <TableCell className="text-center">
-              <Input
-                type="number"
-                id="quantity"
-                min="1"
-                placeholder="Quantity"
-                onChange={(e) => handleChange(e.target.value, product.id)}
-                defaultValue={product.quantity.toString() || ""}
-              />
-            </TableCell>
-            <TableCell className="text-right">{`$${product.total}`}</TableCell>
-            <TableCell className="text-center">
-              <Button
-                onClick={() => {
-                  removeProduct(parseInt(product.id));
-                  toast({
-                    description: `${product.title} removed from cart`,
-                  });
-                }}
-                variant="ghost"
-                size="icon"
-                className="w-full"
-              >
-                <FaTrash className="md:h-4 md:w-4" />
-              </Button>
-            </TableCell>
+    <div className="flex flex-col justify-center items-center space-y-3">
+      <Table className="max-w-[500px]">
+        <TableCaption>All transactions are final</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="md:w-[100px]">Item </TableHead>
+            <TableHead className="md:w-[200px]">Description </TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className=""></TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">{`$${cart.total - 315}`}</TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {filteredProducts.map((product: Product) => (
+            <TableRow key={product.title}>
+              <TableCell className="">
+                <div className=" aspect-square flex justify-center items-center bg-black overflow-hidden">
+                  <Image
+                    src={product.thumbnail}
+                    width={100}
+                    height={100}
+                    alt={product.title}
+                    className="object-cover"
+                  />
+                </div>
+              </TableCell>
+              <TableCell className="font-medium">{product.title}</TableCell>
+              <TableCell className="text-center">
+                <Input
+                  type="number"
+                  id="quantity"
+                  min="1"
+                  placeholder="Quantity"
+                  onChange={(e) => handleChange(e.target.value, product.id)}
+                  defaultValue={product.quantity.toString() || ""}
+                />
+              </TableCell>
+              <TableCell className="text-right">{`$${product.total}`}</TableCell>
+              <TableCell className="text-center">
+                <Button
+                  onClick={() => {
+                    removeProduct(parseInt(product.id));
+                    toast({
+                      description: `${product.title} removed from cart`,
+                    });
+                  }}
+                  variant="ghost"
+                  size="icon"
+                  className="w-full"
+                >
+                  <FaTrash className="md:h-4 md:w-4" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">{`$${
+              cart.total - 315
+            }`}</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            className="self-center w-full"
+            disabled={cart.total - 315 === 0 || cart.total - 315 < 0}
+          >
+            Check Out
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>You are checking out</AlertDialogTitle>
+            <AlertDialogDescription>
+              All purchases are final. Are you sure you want to proceed?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
   );
 }
